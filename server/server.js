9e163a09364d03dd97119dcf6c0739558ff79333
Tommaso Tadiello <tommaso.attimi@pm.me>
2024-05-21 15:36:15 +0200

    basic notes app

--- server/server.js
+++ server/server.js
@@ -4,16 +4,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB Atlas
-const dbURI = "your mongodb connection string";
+const dbURI = process.env.MONGO_URI;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Define Note model
